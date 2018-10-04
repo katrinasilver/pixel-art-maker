311f9676b3923b5df7d069d3b4730282d04dd4e1
Katrina Agustin <katrina.agustin@gmail.com>
2018-10-03 20:15:52 -0700

    created pixel art maker with event listeners

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+for (let i = 0; i < 7236; i++) {
+    const canvas = document.querySelector('.container');
+    const square = document.querySelector('.tiles');
+    const swatch = document.querySelector('.swatch')
+    const reset = document.querySelector('.reset');
+    const box = document.createElement('div');
+
+    // create grid
+    box.className = 'tiles';
+    canvas.appendChild(box);
+
+    // prepare the functions!!!
+    const reseT = () => box.style.backgroundColor = 'transparent';
+    const coloR = () => box.style.backgroundColor = swatch.value;
+
+    // brush behaviour
+    canvas.addEventListener('mousedown', (e) => {
+        box.addEventListener('mouseover', coloR, false);
+    })
+
+    // when i want to stop painting
+    canvas.addEventListener('mouseup', (e) => {
+        box.removeEventListener('mouseover', coloR, false);
+    });
+
+    //reset, why erase one pixel when you can erase all?!!!
+    reset.addEventListener('click', reseT, false);
+}
