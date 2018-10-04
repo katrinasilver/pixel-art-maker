c84653a11e50b0e112b3b41fcdf709ec17836560
Katrina Agustin <katrina.agustin@gmail.com>
2018-10-03 22:11:36 -0700

    preventDefault

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ for (let i = 0; i < 7236; i++) {
     const reseT = () => box.style.backgroundColor = 'transparent';
     box.className = 'tiles'; // make the tiles visible
     canvas.appendChild(box); // add the tiles to the DOM
-    canvas.addEventListener('mousedown', (e) => box.addEventListener('mouseover', coloR, false));
+    canvas.addEventListener('mousedown', (e) => { // start painting
+        e.preventDefault() // hijack the mouse drag
+        box.addEventListener('mouseover', coloR, false)
+    });
     canvas.addEventListener('mouseup', (e) => box.removeEventListener('mouseover', coloR, false));
     document.querySelector('.reset').addEventListener('click', reseT, false);
 }
